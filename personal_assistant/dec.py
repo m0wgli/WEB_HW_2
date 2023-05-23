b79d6c4c0dab40ef0bcfd3d64954261d77b5dbf1
m0wgli <rodiontiutiunyk@gmail.com>
2023-05-23 15:40:02 +0300

    first commit

---
+++ personal_assistant/dec.py
@@ -0,0 +1,13 @@
+from functools import wraps
+
+
+def input_error(func):
+    @wraps(func)
+    def wrapper(*args):
+        try:
+            return func(*args)
+        except (KeyError, ValueError, IndexError) as exc:
+            result = f"Я повернув тебе назад в меню тому що: {str(exc)}"
+            return result
+
+    return wrapper
